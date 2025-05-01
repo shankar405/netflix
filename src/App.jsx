978d6ab96995219baa60994a6c33d1db0431db79
shankar405 <shankarmalviya067@gmail.com>
2025-05-01 07:35:04 +0530

    configured tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className='text-3xl font-bold text-red-200'>Lets build netflix gpt</div>
+        
+    </>
+  )
+}
+
+export default App
